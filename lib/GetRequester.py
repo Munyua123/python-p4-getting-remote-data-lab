0996d787ff675487b5463bb23d6ada4a47615e38
Earl <earlmunyua96@gmail.com>
2024-01-26 08:18:03 +0300

    remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,24 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data = json.loads(self.get_response_body())
+            
+        occupation_list = [
+                {'name': entry.get("name", ""), 'occupation': entry.get("occupation", "")}
+                for entry in data
+            ]
+
+        return occupation_list
+    
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+
+
+
+
+
+
